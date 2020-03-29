36cffb33db08024c1de2da5c241df193bd5945b9
João Paulo <joaopaulosil@yahoo.com.br>
2020-03-29 17:26:07 -0300

    estava dando erro de requiscao por dentro do docker ao packgist | adicao do eloquent ao projeto

---
+++ bootstrap/app.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once 'eloquent.php';
+
+date_default_timezone_set('America/Sao_Paulo');
---
+++ bootstrap/eloquent.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+
+
+$capsule = new Capsule();
+
+$dbConfig = CONFIG['databases']['mysql'];
+
+$capsule->addConnection($dbConfig);
+$capsule->setEventDispatcher(new Dispatcher(new Container()));
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
